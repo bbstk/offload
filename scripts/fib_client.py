721fc3b2a14abed599cb2e2cf7db5ee5018ba151
Ivan Valkov <iv.v.valkov@gmail.com>
2017-12-20 12:38:00 +0000

    Fib client asks several servers

--- scripts/fib_client.py
+++ scripts/fib_client.py
@@ -3,12 +3,33 @@
 import sys
 import rospy
 from offload.srv import *
+from time import time
 
 def fib_client(n):
-    rospy.wait_for_service('fib_solver')
+    #rospy.wait_for_service('fib_solver')
     try:
-        fib = rospy.ServiceProxy('fib_solver', FibSolver)
-        resp1 = fib(n)
+        fib1 = rospy.ServiceProxy('fib_solver_virtual-pi-1', FibSolver)
+        fib2 = rospy.ServiceProxy('fib_solver_virtual-pi-2', FibSolver)
+        fib3 = rospy.ServiceProxy('fib_solver_pi-1', FibSolver)
+        for x in range(0, 10):      
+            if x%3 == 0:
+                print "Asking virtual-pi-1"
+                start = time()
+                resp1 = fib1(n)
+                end = time()
+                print "It took %d secs"%(end-start)
+            elif x%3 == 1:
+                print "Asking virtual-pi-2"
+                start = time()
+                resp1 = fib2(n)
+                end = time()
+                print "It took %d secs"%(end-start)
+            else:
+                print "Asking pi-1"
+                start = time()
+                resp1 = fib3(n)
+                end = time()
+                print "It took %d secs"%(end-start)
         return resp1.result
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
