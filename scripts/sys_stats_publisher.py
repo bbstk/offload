1523c6fb8f1e3a9922f6400b2f1e9a5d442041bb
Ivan Valkov <iv.v.valkov@gmail.com>
2018-01-15 23:56:50 +0000

    Added system stats publisher

---
+++ scripts/sys_stats_publisher.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+import offload.msg
+import socket
+import psutil
+
+def reportStats():
+    pub = rospy.Publisher('stats_' + socket.gethostname(), offload.msg.SystemStats)
+    rospy.init_node('stats_publisher_' + socket.gethostname(), anonymous=True)
+    rate = rospy.Rate(0.5) # 10hz
+    while not rospy.is_shutdown():
+        msg = offload.msg.SystemStats()
+        msg.cpuUsage = psutil.cpu_percent(interval=1)
+        msg.availableMemory = (psutil.virtual_memory().available) / 1024 / 1024
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        reportStats()
+    except rospy.ROSInterruptException:
+        pass
