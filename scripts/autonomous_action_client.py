3306395facfc49c661494bee2941f555f3a2a938
Ivan Valkov <iv.v.valkov@gmail.com>
2018-02-16 12:07:15 +0000

    Modified the cost model to include a table of computation for fib32

--- scripts/autonomous_action_client.py
+++ scripts/autonomous_action_client.py
@@ -5,6 +5,7 @@ from actionlib_msgs.msg import *
 from offload.srv import *
 import offload.msg
 import socket
+import math
 
 class AutonomousActionClient:
     def __init__(self, ActionSpec):
@@ -35,21 +36,44 @@ class AutonomousActionClient:
         return client.send_goal(goal, transition_cb, feedback_cb)
 
 
+fibPerformanceMap = {
+  32:{
+      25: 14.2,
+      50: 14.3,
+      75: 15.6,
+      100: 16.2,
+  }
+}
+
+
 # return the best server to use for a specific goal
 def best_server(goal):
     blackboard = rospy.ServiceProxy('stats_reporter_' + socket.gethostname(), Blackboard)
     load_info = blackboard("")
-    
+
     rospy.loginfo("Load info %r", load_info)
 
-    current_best = offload.msg.SystemStats()
-    current_best.name = socket.gethostname()
-    current_best.cpuUsage = 900.0
-    current_best.availableMemory = 0
+    current_node = socket.gethostname();
+
+    current_best = {}
+    current_best["name"] = socket.gethostname()
+    current_best["timeTotal"] = 99999
+
+    # current_best = offload.msg.SystemStats()
+    # current_best.name = socket.gethostname()
+    # current_best.cpuUsage = 900.0
+    # current_best.availableMemory = 0
 
     for node in load_info.result:
-        if node.cpuUsage < current_best.cpuUsage:
-            current_best.name = node.name
-            current_best.cpuUsage = node.cpuUsage
-            current_best.availableMemory = node.availableMemory
-    return "fib_server_" + current_best.name
+        timeComp = fibPerformerMap[goal.order][math.ceil(node.cpuUsage / 25.0) * 25]
+        rospy.loginfo(node.name)
+        rospy.loginfo(timeComp)
+        timeComm = 0;
+        if node.name != current_node:
+            timeComm = 0.006;
+        timeTotal = timeComp + timeComm
+        if timeTotal < current_best["timeTotal"]:
+            current_best["name"] = node.name
+            current_best["timeTotal"] = timeTotal
+
+    return "fib_server_" + current_best["name"]
