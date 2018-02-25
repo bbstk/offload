3d484c723c06dd3f58c6aef561d99e8227108806
Ivan Valkov <iv.v.valkov@gmail.com>
2018-02-25 18:08:54 +0000

    Added RoutePlan action with client and server

---
+++ scripts/route_plan_client.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+from __future__ import print_function
+from time import time
+import rospy
+import sys
+import socket
+
+import actionlib
+from actionlib_msgs.msg import *
+import offload.msg
+from autonomous_action_client import AutonomousActionClient
+
+node_addresses = ['pi1', 'pi2', 'pi3', 'pi4', 'pi5']
+
+def single_server(cX, cY, tX, tY, steps):
+    client = actionlib.ActionClient('route_planner_' + socket.gethostname(), offload.msg.RoutePlanAction)
+    client.wait_for_server()
+    goal = offload.msg.RoutePlanGoal(cX=cX, cY=cY, tX=tX, tY=tY, steps=steps)
+    handler = client.send_goal(goal)
+    while handler.get_goal_status() != GoalStatus.SUCCEEDED:
+        rospy.sleep(0.2)
+
+    return handler.get_result()
+
+def fibonacci_client_amp_server(n, repetitions):
+    aac = AutonomousActionClient(offload.msg.RoutePlanAction)
+    goal = offload.msg.RoutePlanGoal(cX=cX, cY=cY, tX=tX, tY=tY, steps=steps)
+
+    sendStart = time()
+    handler = aac.send_goal(goal)
+    sendEnd = time()
+    rospy.loginfo("Sent all goals in %d seconds", sendEnd - sendStart)
+
+    while handler.get_goal_status() != GoalStatus.SUCCEEDED:
+        rospy.sleep(0.2)
+
+    return handler.get_result() 
+
+def usage():
+    return "%s [cX] [xY] [tX] [tY] [steps] [single|amp]"%sys.argv[0]
+
+if __name__ == '__main__':
+    if len(sys.argv) == 7 and sys.argv[6] in ['single', 'amp']:
+        cX = int(sys.argv[1])
+        cY = int(sys.argv[2])
+        tX = int(sys.argv[3])
+        tY = int(sys.argv[4])
+        steps = int(sys.argv[5])
+        mode = sys.argv[6]
+    else:
+        print (usage())
+        sys.exit(1)
+    try:
+        rospy.init_node('route_plan_client', anonymous=True)
+        result = []
+        if mode == 'single':
+            result = single_server(cX, cY, tX, tY, steps)
+        elif mode == 'rr':
+            result = round_robin_server(cX, cY, tX, tY, steps)
+        elif mode == 'amp':
+            result = amp_server(cX, cY, tX, tY, steps)
+        else:
+            print (usage())
+            sys.exit(1)
+        print("Result: ", result.result)
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
---
+++ scripts/route_plan_server.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+from threading import Thread
+from time import time
+import subprocess
+
+import rospy
+import actionlib
+
+import offload.msg
+
+import socket
+
+class RoutePlanServer(object):
+    # create messages that are used to publish feedback/result
+    # _feedback = offload.msg.FibonacciFeedback()
+    # _result = offload.msg.FibonacciResult()
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.ActionServer(self._action_name, offload.msg.RoutePlanAction, goal_cb=self.goal_cb, auto_start = False)
+        self._as.start()
+      
+    def goal_cb(self, goalHandle):
+        goalHandle.set_accepted()
+        rospy.loginfo("Accepted new goal")
+        def updateStatus(orderNumber):
+            feedback = offload.msg.RoutePlanActionFeedback()
+            #rospy.loginfo("Attributes: %s", vars(feedback))
+            # feedback.currentOrder = orderNumber
+            # goalHandle.publish_feedback(feedback)
+        
+        def completed(finalResult):
+            status = goalHandle.get_goal_status().status
+            #updateStatus(sequence)
+
+            result = offload.msg.RoutePlanResult()
+            result.result = finalResult
+            
+            if status == actionlib.GoalStatus.ACTIVE:
+                goalHandle.set_succeeded(result)
+            elif status == actionlib.GoalStatus.PREEMPTED:
+                goalHandle.set_preempted(result)
+            else:
+                goalHandle.set_canceled(result)
+        
+        def findShortestRoute(goalHandle, statusCB, doneCB):
+            start = time()
+            goal = goalHandle.get_goal()
+            rospy.loginfo('%s: Calculating shortest path from (%i,%i) to (%i,%i)' % (self._action_name, goal.cX, goal.cY, goal.tX, goal.tY))
+
+            subprocess.call("mzn2fzn /home/ubuntu/route-finder-cp/route-finder.mzn /home/ubuntu/route-finder-cp/data.dzn -D\"steps="+goal.steps+";cX="+goal.cX+";cY="+goal.cY+";tX="+goal.tX+";tY="+goal.tY+"\" -o "+ str(start) + ".fzn", shell=True)
+            result = subprocess.check_output("fzn-gecode -p 4 "+ str(start) +".fzn",shell=True)
+            
+            end = time()
+            rospy.loginfo('%s: Calculating shortest path from (%i,%i) to (%i,%i) in %d seconds' % (self._action_name, goal.cX, goal.cY, goal.tX, goal.tY, end-start))
+            doneCB(result)
+
+        Thread(target=findShortestRoute, args=(goalHandle, updateStatus, completed)).start()  
+    
+if __name__ == '__main__':
+    #rospy.init_node('fibonacci')
+    rospy.init_node('route_planner_' + socket.gethostname())
+    server = RoutePlanServer(rospy.get_name())
+    rospy.spin()
