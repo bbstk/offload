a11cfb23d284e6e1e31502c2a967173eac71d554
Ivan Valkov <iv.v.valkov@gmail.com>
2018-03-11 23:30:07 +0000

    Added the best server lookup to the AAC class

--- scripts/route_plan_aac_ft.py
+++ scripts/route_plan_aac_ft.py
@@ -27,7 +27,7 @@ class RoutePlanAutonomousActionClientFT:
         self.goal = goal
         self.done_cb = done_cb
         # get best action server to use
-        self.server_to_use = best_server(goal)
+        self.server_to_use = self._best_server(goal)
         rospy.loginfo("Best server name: " + self.server_to_use)
         self.client = actionlib.SimpleActionClient("route_planner_" + self.server_to_use, self.action_spec)
         #TODO: wait a certain amount, if timeout -> send to another server
@@ -55,44 +55,42 @@ class RoutePlanAutonomousActionClientFT:
                 self.send_goal(self.goal, self.done_cb)
                 return
 
+    # return the best server to use for a specific goal
+    def _best_server(goal):
+        load_info = self.blackboard("",False)
 
+    #    rospy.loginfo("Load info %r", load_info)
 
-# return the best server to use for a specific goal
-def best_server(goal):
-    load_info = self.blackboard("",False)
+        current_node = socket.gethostname()
 
-#    rospy.loginfo("Load info %r", load_info)
+        current_best = {}
+        current_best["name"] = socket.gethostname()
+        #current_best["busyCores"] = 4
+        current_best["timeTotal"] = 99999     
 
-    current_node = socket.gethostname()
+        # current_best = offload.msg.SystemStats()
+        # current_best.name = socket.gethostname()
+        # current_best.cpuUsage = 900.0
+        # current_best.availableMemory = 0
+     #   rospy.loginfo(goal)
+        
+        random.shuffle(load_info.result)
+        for node in load_info.result:
+            # timeComp = fibPerformanceMap[goal.order][int(math.ceil(node.cpuUsage / 25.0) * 25)]
+            rospy.loginfo(node.name)
+            rospy.loginfo("cpu usage: %f"%(node.cpuUsage))
+        # rospy.loginfo(timeComp)
+            timeComp = 10 + 0.1 * int(math.ceil(node.cpuUsage / 10.0) * 10)
+        timeComm = 0;
+            if node.name != current_node:
+                timeComm = 0.006;
+            timeTotal = timeComp + timeComm
+    #        busyCores = 100/int(math.ceil(node.cpuUsage / 25.0) * 25)
+    #	rospy.loginfo("busy cores: %d"%(busyCores))
+        rospy.loginfo("time total: %f"%(timeTotal)) 
+            if timeTotal < current_best["timeTotal"]:# or (timeTotal == current_best["timeTotal"] and random.randint(0,1) == 1):
+    #	if busyCores < current_best["busyCores"] or (busyCores == current_best["busyCores"] and random.randint(0,1) == 1):
+                current_best["name"] = node.name
+                current_best["timeTotal"] = timeTotal
 
-    current_best = {}
-    current_best["name"] = socket.gethostname()
-    #current_best["busyCores"] = 4
-    current_best["timeTotal"] = 99999     
-
-    # current_best = offload.msg.SystemStats()
-    # current_best.name = socket.gethostname()
-    # current_best.cpuUsage = 900.0
-    # current_best.availableMemory = 0
- #   rospy.loginfo(goal)
-    
-    random.shuffle(load_info.result)
-    for node in load_info.result:
-        # timeComp = fibPerformanceMap[goal.order][int(math.ceil(node.cpuUsage / 25.0) * 25)]
-        rospy.loginfo(node.name)
-        rospy.loginfo("cpu usage: %f"%(node.cpuUsage))
-	# rospy.loginfo(timeComp)
-        timeComp = 10 + 0.1 * int(math.ceil(node.cpuUsage / 10.0) * 10)
-	timeComm = 0;
-        if node.name != current_node:
-            timeComm = 0.006;
-        timeTotal = timeComp + timeComm
-#        busyCores = 100/int(math.ceil(node.cpuUsage / 25.0) * 25)
-#	rospy.loginfo("busy cores: %d"%(busyCores))
-	rospy.loginfo("time total: %f"%(timeTotal)) 
-        if timeTotal < current_best["timeTotal"]:# or (timeTotal == current_best["timeTotal"] and random.randint(0,1) == 1):
-#	if busyCores < current_best["busyCores"] or (busyCores == current_best["busyCores"] and random.randint(0,1) == 1):
-            current_best["name"] = node.name
-            current_best["timeTotal"] = timeTotal
-
-    return current_best["name"]
+        return current_best["name"]
